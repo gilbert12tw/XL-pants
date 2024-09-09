60c3d0f543cf9a9dc02a1c36fb1bae8eb7ff7398
Littlepants <gilbert12.tw@gmail.com>
2024-09-09 13:30:43 +0800

    Fix: vimrc

--- codes/1_Basic/vimrc.cpp
+++ codes/1_Basic/vimrc.cpp
@@ -1,7 +1,7 @@
-set nu ai hls et ru ic is sc cul
+set nu ai hls et ru ic is sc cul si
 set re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
 set timeoutlen=300
 hi cursorline cterm=none
 "Select region and type :Hash to hash your selection."
 ca Hash w !cpp -dD -P -fpreprocessed \| tr -d '[:space:]' \| md5sum \| cut -c-6
-map <F9> :w !clear && g++ -std=c++17 -Ddebug -O2 -Wall -lm -g % && ./a.out<CR>
+map <F9> :w<CR> !clear && g++ -std=c++17 -Ddebug -O2 -Wall -lm -g % && ./a.out<CR>
